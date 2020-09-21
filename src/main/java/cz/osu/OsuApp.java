7b196eaf4a9b0e21bb3e784d204ce564196acf76
ph4nt0m_gh0sT <ph4nt0mgh0sT17@gmail.com>
2020-09-21 10:10:23 +0200

    Update OsuApp.java

--- src/main/java/cz/osu/OsuApp.java
+++ src/main/java/cz/osu/OsuApp.java
@@ -7,11 +7,27 @@ import lombok.extern.slf4j.Slf4j;
 public class OsuApp {
 
     public static void main(String[] args) {
-        log.info("The application started.");
 
-        System.out.println(IntroductionDisplayer.renderIntroduction());
+        printIntroductionText();
 
-        log.info("The application ended.");
+        // 7OPR1 - 1st lecture
+        Counter counter = new Counter(0,"OSU COUNTER");
+
+        System.out.println(counter);
+        counter.add(52);
+        System.out.println(counter);
+        counter.remove(14);
+        System.out.println(counter);
+
+        DayCounter dayCounter = new DayCounter(15,"neco");
+        System.out.println(dayCounter.dayOfWeek());
+    }
+
+    private static void printIntroductionText() {
+        System.out.println("" +
+                "====================================\n" +
+                "               ffs                  \n" +
+                "====================================\n");
     }
 
 
