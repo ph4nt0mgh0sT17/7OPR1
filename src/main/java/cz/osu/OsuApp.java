72ed7faf79d990ddec2c5a2c00286291c53e8cb6
David Miko <ph4nt0mgh0sT17@gmail.com>
2020-10-12 09:45:11 +0200

    Added new branch for 3rd lecture.

--- src/main/java/cz/osu/OsuApp.java
+++ src/main/java/cz/osu/OsuApp.java
@@ -11,23 +11,7 @@ public class OsuApp {
     public static void main(String[] args) {
         System.out.println(IntroductionDisplayer.renderIntroduction());
 
-        GuiEngine guiEngine = new GuiEngine();
-
-        Point point1 = new Point(50, 50, Color.RED, guiEngine);
-
-        guiEngine.addShape(new Rectangle(50,50,100,100,Color.RED));
-        guiEngine.printEnvironment();
-        /*for (int i = 0; i < 600; i++) {
-            point1.draw();
-            guiEngine.pause(1);
-            point1.shiftCoordinates(1, 0);
-        }
-
-        for (int i = 0; i <= 600; i++) {
-            point1.erase();
-            guiEngine.pause(1);
-            point1.shiftCoordinates(-1, 0);
-        }*/
+        // See study materials
     }
 
 
--- src/main/java/cz/osu/Shape.java
+++
@@ -1,8 +0,0 @@
-package cz.osu;
-
-/**
- * The interface representation of GUI shape that can be drawn in the GUI.
- */
-public interface Shape {
-    void draw(GuiEngine guiEngine);
-}
